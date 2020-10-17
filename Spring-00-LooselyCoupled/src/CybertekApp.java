b38cbf6cfffe215c6340c0af4e0d794fe7e9d4a7
mrsteel44 <asimcelikk@gmail.com>
2020-10-17 16:52:42 +0100

    loosely coupled

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,15 @@
+import implementation.Mentor;
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class CybertekApp {
+
+    public static void main(String[] args) {
+        //FullTimeMentor fullTimeMentor = new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
+        MentorAccount mentor = new MentorAccount(partTimeMentor);
+
+        mentor.manageAccount();
+    }
+}
